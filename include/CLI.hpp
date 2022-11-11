8e1b0c1805d81d2061c9661aceffc0a536057429
Isaac Fonseca <isaac.fonsecasegura@ucr.ac.cr>
2022-11-11 09:04:24 -0600

    Previamente ProgramInterface
    Signed-off-by: Isaac Fonseca <isaac.fonsecasegura@ucr.ac.cr>

---
+++ include/CLI.hpp
@@ -0,0 +1,35 @@
+/**
+ * @file ParallelDeWAFF.hpp
+ * @author David Prado (davidp)
+ * @date 2015-11-05
+ * @author Isaac Fonseca (isaac.fonsecasegura@ucr.ac.cr)
+ * @date 2022-11-06
+ * @brief 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef PROGRAM_INTERFACE_HPP_
+#define PROGRAM_INTERFACE_HPP_
+
+#include <string>
+#include <unistd.h>
+#include <iostream>
+#include "FileProcessor.hpp"
+
+/**
+ * @brief In charge of displaying the CLI of the program, including the help commands.
+ * It takes the CLI instructions to process the file. This is why this class inherits from
+ * the FileProcessor class.
+ */
+class CLI : protected FileProcessor {
+public:
+	CLI(int argc, char** argv);
+	int run();
+
+private:
+	std::string programName;
+	void help();
+};
+
+#endif /* PROGRAM_INTERFACE_HPP_ */
