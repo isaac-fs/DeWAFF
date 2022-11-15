7180397097b1f7dda8a570c489e98d330d44c2d4
Isaac Fonseca <isaac.fonsecasegura@ucr.ac.cr>
2022-11-15 12:09:49 -0600

    DeWAFF con DBF ya está integrado
    Signed-off-by: Isaac Fonseca <isaac.fonsecasegura@ucr.ac.cr>

--- algorithms/ParallelDeWAFF-master/Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lgomp -lopencv_core -lopencv_videoio -lopencv_imgcodecs -lopencv_imgproc -lopencv_highgui
-
