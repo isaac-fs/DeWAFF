d3ab1cec466e44162e343576cf3005709ac591b4
Isaac Fonseca Segura <isaac.fonsecasegura@ucr.ac.cr>
2022-12-03 21:37:42 -0600

    Updated class hierarchy

--- docs/class_filters.js
+++ docs/class_filters.js
@@ -3,6 +3,5 @@ var class_filters =
     [ "BilateralFilter", "class_filters.html#a78a4c57fc753e6de4cca5c67a494afa6", null ],
     [ "GuidedFilter", "class_filters.html#a9eb2156050f10f40dfafa934f8f9ded8", null ],
     [ "NonLocalMeansFilter", "class_filters.html#a02eafe8690dcd01dde0f81b1c5966b44", null ],
-    [ "ScaledBilateralFilter", "class_filters.html#aa6faf8cd342f97cb42f303f12cf36702", null ],
-    [ "lib", "class_filters.html#a585c34a07148c8a772e04fb38ae22569", null ]
+    [ "ScaledBilateralFilter", "class_filters.html#aa6faf8cd342f97cb42f303f12cf36702", null ]
 ];
--- docs/hierarchy.js
+++ docs/hierarchy.js
@@ -1,8 +1,7 @@
 var hierarchy =
 [
-    [ "Filters", "class_filters.html", [
-      [ "DeWAFF", "class_de_w_a_f_f.html", null ]
-    ] ],
+    [ "DeWAFF", "class_de_w_a_f_f.html", null ],
+    [ "Filters", "class_filters.html", null ],
     [ "ProgramInterface", "class_program_interface.html", null ],
     [ "Utils", "class_utils.html", [
       [ "Timer", "class_timer.html", null ]
